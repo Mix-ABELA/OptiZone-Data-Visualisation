7d721ef99c5203ed8d43b04aae48cdbcef6ee416
Mix-ABELA <abelamichel@gmail.com>
2025-10-13 20:50:26 +0200

    created the CSV data processor module

---
+++ src/csv_handler/csv_data_processor.py
@@ -0,0 +1,23 @@
+"""
+CODE: 
+This module is part of the CSV (Comma Seperated Values) handler library.
+Its job is to process & modify the data sorted inside relevant lists, in order to make it compatible
+with the python project
+"""
+
+# INCLUDE LIBRARIES
+
+
+# DEFINE USEFUL FUNCTIONS
+
+
+
+# INTERNAL CODE TESTING
+
+if __name__ == '__main__':  # this part will only run when the script is called manually in terminal
+
+    try:
+        pass
+
+    finally:
+        print("PROGRAM ENDED SUCCESSFULLY")
