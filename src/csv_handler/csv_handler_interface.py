ab49fd38b891c1d93d2b34dd8d5e00f2ae641fb0
Mix-ABELA <abelamichel@gmail.com>
2025-11-16 11:16:00 +0100

    added the CSV handler interface getter for processed data access in the main project code

--- src/csv_handler/csv_handler_interface.py
+++ src/csv_handler/csv_handler_interface.py
@@ -16,6 +16,7 @@ class csv_data_handler:
     def __init__(self, csv_file_path=""): # class constructor
         try:
             self._raw_data = csv_extractor.load_data(csv_file_path) # load data into extractor instance
+            self.processed_data = [] # create empty list to store processed data 
         except:
             print("Failed to load dataset file... TRY AGAIN!")
             exit(0)
@@ -24,6 +25,13 @@ class csv_data_handler:
     def extract_data(self): # extract dataset lists & process data values
         sorted_data_list = csv_extractor.sort_data(self._raw_data) # extract different lists from raw data
         processed_data_list = csv_data_processor.filter_data(sorted_data_list) # process data ready for computation
+        self.processed_data = processed_data_list # save processed data in class object
+    
+    def get_processed_data(self): # returns the processed data list
+        if self.processed_data == []: # check if list is empty (no computation made yet)
+            raise ValueError("no processed data list has been created yet...")
+        else:
+            return self.processed_data
 
 
 # INTERNAL CODE TESTING
@@ -37,6 +45,7 @@ if __name__ == '__main__':  # this part will only run when the script is called 
 
         CSV_Handler = csv_data_handler(csv_file_name)
         CSV_Handler.extract_data()
+        print(CSV_Handler.get_processed_data().conc_time_list)
 
     finally:
         print("PROGRAM ENDED SUCCESSFULLY")
